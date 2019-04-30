1b50e4a9917a36d72f13db0c0c52c3d8bad83ed5
Uwana <uwikaiddi@gmail.com>
2019-04-29 21:04:47 -0500

    Alter About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Final RenPy Code</h1>
+     <textarea name="final" rows="50" cols="30">
+        {{ label.output }}
+      </textarea>
   </div>
 </template>
+
+<script>
+  import axios from "axios";
+  export default {
+  data: function() {
+    return {
+      message: "Hello to Vue.js!",
+      labels: [],
+    };
+  },
+  return_final: function() {
+    axios.get("/api/labels/final").then(response => {
+      this.labels = response.data;
+    })
+  },
+  methods: {
+    addNewFinal: function() {
+      console.log('printing final...');
+      var params = {
+        input_name: this.newProductName,
+        input_price: this.newProductPrice,
+        input_description: this.newProductDescription
+      }
+    }
+  }
+};
+</script>
