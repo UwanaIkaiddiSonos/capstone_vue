ecdaf864ef8b4f6a26cd29adcfd56259be625320
Uwana <uwikaiddi@gmail.com>
2019-04-30 15:21:47 -0500

    Add connections to backend in Home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,20 @@
   <div class="home">
     <h1>{{ message }}</h1>
 
+    <form>
+      <p>First Name: <input type="text" v-model="newCharacter.firstName"></p>
+      <p>Last Name: <input type="text" v-model="newCharacter.lastName"></p>
+      <p>Color: <input type="color" v-model="newCharacter.color"></p>
+    <button v-on:click.prevent="addCharacter()">Create character</button>
+    </form>
+<hr>
     <form> <!--Label form-->
       <p>Scene: <select>
-        <option v-for="scene in background_images">{{ background_image.url }}</option>
+        <option v-for="background_image in background_images">{{ background_image.name }}</option>
       </select></p>
 
       <p>Transition:  <select name="transition">
-        <option value="fade">Fade</option>
-        <option value="dissolve">Dissolve</option>
-        <option value="swipe">Swipe</option>
+        <option v-for="transition in transitions">{{ transition.name }}</option>
       </select></p>
     </form>
 <hr>
@@ -29,15 +34,11 @@
     <form> <!--Menu form-->
       <p>Intro line: <input type="text" name="intro_line"></p>
       <p>Choice 1 label: <select name="labels">
-        <option value="burger">Burger</option>
-        <option value="ramen">Ramen</option>
-        <option value="tacos">Taco</option>
+        <option v-for="label in labels">{{ label.name }}</option>
       </select></p>
       <p>Choice 1 text: <input type="text" name="choice1"></p>
       <p>Choice 2 label: <select name="labels">
-        <option value="burger">Burger</option>
-        <option value="ramen">Ramen</option>
-        <option value="tacos">Taco</option>
+        <option v-for="label in labels">{{ label.name }}</option>
       </select></p>
       <p>Choice 2 text: <input type="text" name="choice1"></p>
       <input type="submit" value="Add Choice">
@@ -60,14 +61,22 @@ export default {
       message: "Hello world!",
       characters: [],
       newCharacter: {
-        first_name: "",
-        last_name: "",
-        color: ""
+        firstName: "",
+        lastName: "",
+        color: "#000000"
       },
       background_images: [],
-      newBackgroundImage: {
-        url: ""
-      }
+      newScene: {
+        name: ""
+      },
+      labels: [],
+      newLabel: {
+        name: ""
+      },
+      transitions: [],
+      newTransition: {
+        name: ""
+      },
     };
   },
   created: function() {
@@ -76,7 +85,26 @@ export default {
     });
     axios.get("/api/background_images").then(response => {
       this.background_images = response.data;    
+    });
+    axios.get("/api/transitions").then(response => {
+      this.transitions = response.data;    
+    });
+    axios.get("/api/labels").then(response => {
+      this.labels = response.data;    
     })
   },
+  methods: {
+    addCharacter: function() {
+      console.log('editing contact');
+      var params = {
+        input_first_name: this.newCharacter.firstName,
+        input_last_name: this.newCharacter.lastName,
+        input_color: this.newCharacter.color
+      }
+      axios.post("/api/characters", params).then(response => {
+        console.log(response);
+      });
+    }
+  }
 };
 </script>
---
+++ src/views/New.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="new">
+    <h1> {{ message }} </h1>
+    <p>Text</p>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+  export default {
+  data: function() {
+    return {
+      message: "This is the NEW page",
+  //     methods: {
+  //     }
+  //   };
+  // },
+  // created: function() {
+  //   axios.get("/api/drafts").then(response => {
+  //     this.output = response.data;
+  //   })
+  }
+  };
+</script>
