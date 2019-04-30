fa69c6acd8262f1b944ed83a5bc67650e4890524
Uwana <uwikaiddi@gmail.com>
2019-04-30 12:41:46 -0500

    Update Home function get address

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,28 +12,16 @@
   data: function() {
     return {
       output: "",
-      // message: "# Characters used in-game\ndefine s = Character(_(\"Sylvie\"), color=\"#333333\")\nlabel meadow\nscene bg school\nwith fade\n\tshow sylvie green smile\n\ts Hi, I'm Sylvie!\n\tmenu lunch\n\t\tWhere should we eat?\n\t\tGrab a burger\n\t\t\tjump burger\n\t\tGet tacos\n\t\t\tjump tacos\n",
       labels: [],
       characters: [],
       methods: {
-        
       }
     };
   },
   created: function() {
-    axios.get("/api/labels/final").then(response => {
+    axios.get("/api/drafts").then(response => {
       this.output = response.data;
     })
   }
-  // methods: {
-  //   addNewFinal: function() {
-  //     console.log('printing final...');
-  //     var params = {
-  //       input_name: this.newProductName,
-  //       input_price: this.newProductPrice,
-  //       input_description: this.newProductDescription
-  //     }
-  //   }
-  // }
 };
 </script>
