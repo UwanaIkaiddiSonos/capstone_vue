08431a032d1f60dcfc1011050bd1a0fe789f327e
Uwana <uwikaiddi@gmail.com>
2019-05-06 21:29:41 -0500

    Theme changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="wrapper">
   <!--Left segment-->
-  <section class="split-image parallax" data-background="assets/images/module-7.jpg">
+  <section class="split-image parallax" data-background="assets/images/girl_reading.jpg">
     <div class="container-fluid container-custom">
       <br><br>
-              <textarea readonly name="final" rows="20" cols="70">{{ output['output'] }}
-              </textarea>
+        <textarea readonly name="final" rows="10" cols="50">{{ output['output'] }}</textarea>
     </div>
   </section>
   <!--Left segment end-->
@@ -14,81 +13,91 @@
     <!--Insert content here-->
     <section class="module">
       <div class="container-fluid container-custom">
-          <div class="home">
-          <h1>{{ message }}</h1>
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="home">
+              <h1>EasyPy Converter</h1>
+              <form>
+              <!-- Label Section -->
+              <!-- <button v-on:click.prevent="addLabel()">Add new label dropdown</button> -->
+                <!-- <div v-for="oneLabel, $index in listOfLabels"> -->
+                <div class="new1">
+                  <p>Label:  
+                  <select v-model="newLabel.labelName">
+                    <option v-for="label in labels">{{ label.name }}</option>
+                  </select></p>
+                </div>
+              <!-- </div> -->
+              </form>
+              <hr class="new1">
 
-          <form>
-          <!-- Label Section -->
-          <!-- <button v-on:click.prevent="addLabel()">Add new label dropdown</button> -->
-            <!-- <div v-for="oneLabel, $index in listOfLabels"> -->
-            <p>Label:  
-              <select v-model="newLabel.labelName">
-                <option v-for="label in labels">{{ label.name }}</option>
-              </select>
-            </p>
-          <!-- </div> -->
-          </form>
-          <hr>
-
-          <form>
-          <!-- Scene Section -->
-          <!-- <button v-on:click.prevent="addScene()">Add new scene</button> -->
-            <!-- <div v-for="oneScene, $index in listOfScenes"> -->
-            <p>Scene: 
-              <select v-model="newScene.sceneName">
-                <option v-for="background_image in background_images">{{ background_image.scene_name }}</option>
-              </select>
-            </p>
-            <!-- </div> -->
-          <!-- <button v-on:click.prevent="addTransition()">Add new transition</button> -->
-            <!-- <div v-for="oneTransition, $index in listOfTransitions"> -->
-            <p>Transition:  
-              <select v-model="newTransition.transitionName">
-                <option v-for="transition in transitions">{{ transition.name }}</option>
-              </select>
-            </p>
-            <!-- </div> -->
-        </form>
-          <hr>
-
-          <!-- Character Section -->
-            <p>Character: <select v-model="newCharacter.firstName" @change="findCharacterColor()">
-              <option v-for="character in characters">{{ character.first_name }}</option>
-            </select></p>
-            <p>Color: <select v-model="newCharacter.color">
-              <option v-for="character in characters">
-                {{ character.color }}
-              </option>
-            </select></p>
-            <p>Dialogue: <textarea name="dialogue" v-model="newDialogue.dialogueText" rows="2" cols="30"> 
-              Enter your character's dialogue...
-            </textarea>
-            </p>
-            <input type="submit" value="Add more dialogue">
-            <hr>
-
-          <!-- Menu Section -->
-              <p>Menu: <select name="labels" v-model="newMenu.menuName">
-                <option v-for="label in labels">{{ label.name }}</option>
-               </select></p>
-              <p>Intro line: <input type="text" v-model="newMenu.introText" name="intro_line"></p>
-              <p>Choice 1 label: <select name="labels" v-model="newMenu.choiceLabel1">
-                <option v-for="label in labels">{{ label.name }}</option>
-              </select></p>
-              <p>Choice 1 text: <input type="text" name="choice1" v-model="newMenu.choice1"></p>
-              <p>Choice 2 label: <select name="labels" v-model="newMenu.choiceLabel2">
-                <option v-for="label in labels">{{ label.name }}</option>
-              </select></p>
-              <p>Choice 2 text: <input type="text" name="choice1" v-model="newMenu.choice2"></p>
-              <button type="button">Add Choice</button>
+              <form>
+              <!-- Scene Section -->
+              <!-- <button v-on:click.prevent="addScene()">Add new scene</button> -->
+                <!-- <div v-for="oneScene, $index in listOfScenes"> -->
+                <div class="new1">  
+                  <p>Scene: 
+                    <select v-model="newScene.sceneName">
+                      <option v-for="background_image in background_images">{{ background_image.scene_name }}</option>
+                    </select>
+                  </p>
+                <!-- </div> -->
+              <!-- <button v-on:click.prevent="addTransition()">Add new transition</button> -->
+                <!-- <div v-for="oneTransition, $index in listOfTransitions"> -->
+                <p>Transition:  
+                  <select v-model="newTransition.transitionName">
+                    <option v-for="transition in transitions">{{ transition.name }}</option>
+                  </select>
+                </p>
+              </div>
+                <!-- </div> -->
+            </form>
               <hr>
 
-              <!-- <button v-on:click="Generate()">Generate draft</button> -->
-              <form v-on:submit.prevent="Generate()"> <!--Generate button-->
-                <button type="submit">Generate</button>
-              </form>
-            </form>
+              <!-- Character Section -->
+              <div class="new1">
+                <p>Character: <select v-model="newCharacter.firstName" @change="findCharacterColor()">
+                  <option v-for="character in characters">{{ character.first_name }}</option>
+                </select></p>
+                <p>Color: <select v-model="newCharacter.color">
+                  <option v-for="character in characters">
+                    {{ character.color }}
+                  </option>
+                </select></p>
+                <p>Dialogue: <textarea name="dialogue" v-model="newDialogue.dialogueText" rows="2" cols="30"> 
+                  Enter your character's dialogue...
+                </textarea>
+                </p>
+              </div>
+                <!-- <input type="submit" value="Add more dialogue"> -->
+                <hr>
+
+              <!-- Menu Section -->
+              <div class="new1">
+                <p>Menu: <select name="labels" v-model="newMenu.menuName">
+                  <option v-for="label in labels">{{ label.name }}</option>
+                 </select></p>
+                <p>Intro line: <input type="text" v-model="newMenu.introText" name="intro_line"></p>
+                <p>Choice 1 label: <select name="labels" v-model="newMenu.choiceLabel1">
+                  <option v-for="label in labels">{{ label.name }}</option>
+                </select></p>
+                <p>Choice 1 text: <input type="text" name="choice1" v-model="newMenu.choice1"></p>
+                <p>Choice 2 label: <select name="labels" v-model="newMenu.choiceLabel2">
+                  <option v-for="label in labels">{{ label.name }}</option>
+                </select></p>
+                <p>Choice 2 text: <input type="text" name="choice1" v-model="newMenu.choice2"></p>
+                  <!-- <button type="button">Add Choice</button> -->
+              </div>
+                  <hr>
+
+                  <!-- <button v-on:click="Generate()">Generate draft</button> -->
+                  <form v-on:submit.prevent="Generate()"> <!--Generate button-->
+                    <button class="btn-lg" type="submit">Generate</button>
+                  </form>
+                </form>
           </div>
+        </div>
+      </div>
       </div>
     </section>
   </section>
